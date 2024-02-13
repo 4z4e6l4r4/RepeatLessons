7f90f23eb312dcb02783e172afe68d3eb83afa23
Azra <yukselazrael@gmail.com>
2024-02-13 22:47:47 +0300

    Added Project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Repeat_Lessons_Azra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
